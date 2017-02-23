5a5b38f13831d94d6e9556c1c36896cadd57cfd9
César M. Urdaneta <upcesar@gmail.com>
2017-02-23 18:15:47 -0300

    Added encapsulation for HttpClient Class (Rest Service Calling)

---
+++ cleangap.api/Services/HttpClient/ApiCommands.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace cleangap.api.Services.HttpClient
+{
+
+
+    public class ApiCommands : IApiCommand
+    {
+
+        private System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+        private HttpStatusCode _HttpCode = HttpStatusCode.OK;
+        private bool _isSuccess = false;
+
+        public HttpStatusCode HttpCode { get { return _HttpCode; } }
+        public bool IsSucess { get { return _isSuccess; } }
+        
+        public ApiCommands(string pURI)
+        {
+            InitializeHttp(pURI);
+        }
+
+        public ApiCommands(string pURI, string ApiKey)
+        {
+            InitializeHttp(pURI);
+            // Api Key in Base64 format. IE: "dXBjZXNhcnx0ZXN0MTIzNA=="
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", ApiKey);
+
+        }
+
+        private void InitializeHttp(string URI)
+        {
+            // New code:
+            client.BaseAddress = new Uri(URI);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //
+
+        }
+        private void SetStatusHttpResponse(HttpResponseMessage response)
+        {
+            _isSuccess = response.IsSuccessStatusCode;
+            _HttpCode = response.StatusCode;
+        }
+
+        public async Task<string> ExecutePost(string action, object param)
+        {
+            string responseData = string.Empty;
+
+            using (var response = await client.PostAsJsonAsync(action, param))
+            {
+                _isSuccess = false;
+                try
+                {
+                    response.EnsureSuccessStatusCode();
+                    // Handle success
+                    responseData = await response.Content.ReadAsStringAsync();
+
+                }
+                catch (HttpRequestException httpEx)
+                {
+                    // Handle failure
+                    responseData = httpEx.Message;
+                }
+                SetStatusHttpResponse(response);
+
+            }
+
+            return responseData;
+        }
+
+        public async Task<string> ExecuteGet(string action)
+        {
+            string responseData = string.Empty;
+
+            using (var response = await client.GetAsync(action))
+            {
+                _isSuccess = false;
+                try
+                {
+                    response.EnsureSuccessStatusCode();
+                    // Handle success
+                    responseData = await response.Content.ReadAsStringAsync();
+                }
+                catch (HttpRequestException httpEx)
+                {
+                    // Handle failure
+                    responseData = httpEx.Message;
+                }
+
+                SetStatusHttpResponse(response);
+
+            }
+
+            return responseData;
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ cleangap.api/Services/HttpClient/IApiCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cleangap.api.Services.HttpClient
+{
+    interface IApiCommand
+    {
+        bool IsSucess { get; }
+        Task<string> ExecutePost(string action, object param);
+        Task<string> ExecuteGet(string action);
+    }
+}
