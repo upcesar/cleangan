5de3c0f3711bc3c1c82dcf2e36201779d5bea664
César M. Urdaneta <upcesar@gmail.com>
2017-02-23 18:16:23 -0300

    Added NUnit project.

---
+++ cleangap.api.test/TestClass.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cleangap.api.Services.HttpClient;
+
+namespace cleangap.api.Test
+{
+    [TestFixture]
+    public class TestClass
+    {
+        #region Constants
+        private const string RestMockURI = "https://api.spotify.com/";
+        #endregion
+
+        #region Private Test Objects Declaration
+        private ApiCommands api;
+        #endregion
+        #region Private Test Object Initialization
+        [OneTimeSetUp]
+        public void Init()
+        {
+            api = new ApiCommands(RestMockURI);
+        }
+        [OneTimeTearDown]
+        public void Dispose()
+        {
+            api = null;
+        }
+        #endregion
+
+        [Test]
+        public void TestRestTaskGetNotEmpty()
+        {
+            Task<string> answer = api.ExecuteGet("v1/search?q=kmfdm&type=artist");
+
+            answer.Wait();
+
+            Assert.That(answer.Result, Is.Not.Empty);
+        }
+    }
+}
