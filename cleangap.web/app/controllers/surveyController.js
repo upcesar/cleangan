9c4d3316a5450d683c7cd4607687e66239c55eb1
orutrax <orutrax@gmail.com>
2017-03-02 19:37:27 -0300

    Add Username Display

--- cleangap.web/app/controllers/surveyController.js
+++ cleangap.web/app/controllers/surveyController.js
@@ -1,6 +1,8 @@
 'use strict';
 app.controller('surveyController', ['$scope', '$location', 'authService', function ($scope, $location, authService) {
 
+    $scope.user = JSON.parse(window.localStorage.getItem("ls.authorizationData"));
+    
     $scope.logOut = function () {
        authService.logOut();
        $location.path('/');
