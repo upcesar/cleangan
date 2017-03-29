54fa35e609497dd3fd4f7940ac528c0f88c62ec1
César M. Urdaneta <upcesar@gmail.com>
2017-03-29 00:42:54 -0300

    Moved Angular routes to a separated file.

--- cleangap.web/app/app.js
+++ cleangap.web/app/app.js
@@ -1,4 +1,5 @@
 
+'use strict';
 var app = angular.module('cleangap', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar', 'vcRecaptcha']);
 
 var serviceBase = 'http://localhost:51563/';
@@ -14,61 +15,4 @@ app.config(function ($httpProvider) {
 
 app.run(['authService', function (authService) {
     authService.fillAuthData();
-}]);
-
-app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-
-    $routeProvider.when("/", {
-        templateUrl: "app/views/login.html",
-        controller: "loginController",
-    });
-
-    $routeProvider.when("/signin", {
-        templateUrl: "app/views/signin.html",
-        controller: "signinController",
-    });
-
-    $routeProvider.when("/forgot-password", {
-        templateUrl: "app/views/forgot-password.html",
-        controller: "passwordRecoveryController",
-    });
-
-    $routeProvider.when("/password-reset", {
-        templateUrl: "app/views/password-reset.html",
-        controller: "resetPasswordController",
-    });
-
-    $routeProvider.when("/dashboard", {
-        templateUrl: "app/views/dashboard.html",
-    });
-
-    $routeProvider.when("/create-staff", {
-        templateUrl: "app/views/create-staff.html",
-    });
-
-    $routeProvider.when("/create-user", {
-        templateUrl: "app/views/create-user.html",
-    });
-
-    $routeProvider.when("/staff", {
-        templateUrl: "app/views/staff.html",
-    });
-
-    $routeProvider.when("/survey", {
-        templateUrl: "app/views/survey.html",
-        controller: "surveyController",
-    });
-
-    $routeProvider.when("/tokens", {
-        templateUrl: "app/views/tokens.html",
-        controller: "tokensManagerController"
-    });
-
-    $routeProvider.when("/refresh", {
-        templateUrl: "app/views/refresh.html",
-        controller: "refreshController"
-    });
-
-    $routeProvider.otherwise({ redirectTo: "/" });
-    $locationProvider.html5Mode(true);
 }]);
---
+++ cleangap.web/app/routes.js
@@ -0,0 +1,58 @@
+﻿
+'use strict';
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+
+    $routeProvider.when("/", {
+        templateUrl: "app/views/login.html",
+        controller: "loginController",
+    });
+
+    $routeProvider.when("/signin", {
+        templateUrl: "app/views/signin.html",
+        controller: "signinController",
+    });
+
+    $routeProvider.when("/forgot-password", {
+        templateUrl: "app/views/forgot-password.html",
+        controller: "passwordRecoveryController",
+    });
+
+    $routeProvider.when("/password-reset", {
+        templateUrl: "app/views/password-reset.html",
+        controller: "resetPasswordController",
+    });
+
+    $routeProvider.when("/dashboard", {
+        templateUrl: "app/views/dashboard.html",
+    });
+
+    $routeProvider.when("/create-staff", {
+        templateUrl: "app/views/create-staff.html",
+    });
+
+    $routeProvider.when("/create-user", {
+        templateUrl: "app/views/create-user.html",
+    });
+
+    $routeProvider.when("/staff", {
+        templateUrl: "app/views/staff.html",
+    });
+
+    $routeProvider.when("/survey", {
+        templateUrl: "app/views/survey.html",
+        controller: "surveyController",
+    });
+
+    $routeProvider.when("/tokens", {
+        templateUrl: "app/views/tokens.html",
+        controller: "tokensManagerController"
+    });
+
+    $routeProvider.when("/refresh", {
+        templateUrl: "app/views/refresh.html",
+        controller: "refreshController"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/" });
+    $locationProvider.html5Mode(true);
+}]);
