78948fd73c7c3d05a2644dab0763589357534c34
César M. Urdaneta <upcesar@gmail.com>
2017-04-03 12:03:04 -0300

    Removing CORS in Web-Config. Restoring path to "packages" in Projects Files.

--- cleangap.web/app/app.js
+++ cleangap.web/app/app.js
@@ -2,8 +2,8 @@
 'use strict';
 var app = angular.module('cleangap', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar', 'vcRecaptcha']);
 
-//var serviceBase = 'http://localhost:51563/';
-var serviceBase = 'http://cleangap.westcentralus.cloudapp.azure.com:8080/';
+var serviceBase = 'http://localhost:51563/';
+//var serviceBase = 'http://cleangap.westcentralus.cloudapp.azure.com:8080/';
 app.constant('ngAuthSettings', {
     apiServiceBaseUri: serviceBase,
     clientId: 'ngAuthApp'
