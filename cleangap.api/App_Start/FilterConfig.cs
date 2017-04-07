bb37d134967afc1f3f1e4810bb802b5cef258586
César M. Urdaneta <upcesar@gmail.com>
2017-04-07 17:19:30 -0300

    Application Insight Setup.

--- cleangap.api/App_Start/FilterConfig.cs
+++ cleangap.api/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace cleangap.api
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
