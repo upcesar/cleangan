28edf042b1f7aba90f27fde46ba2ebb0325539fd
César M. Urdaneta <upcesar@gmail.com>
2017-07-27 15:57:29 -0300

    Adjustments questions options (Section 6).

--- cleangap.api/Migrations/Seed/SeedQuestionOptions.cs
+++ cleangap.api/Migrations/Seed/SeedQuestionOptions.cs
@@ -470,7 +470,31 @@
 
                     //Do you allow custom price changes on an item level For Managers
                     new question_options() { id = 239, id_question = 100, input_type = "radio", option_text = "Yes", order = 1 },
-                    new question_options() { id = 240, id_question = 100, input_type = "radio", option_text = "No", order = 2 }
+                    new question_options() { id = 240, id_question = 100, input_type = "radio", option_text = "No", order = 2 },
+
+                    //Standard Additional Information: Show Retail Price
+                    new question_options() { id = 239, id_question = 100, input_type = "radio", option_text = "Yes", order = 1 },
+                    new question_options() { id = 240, id_question = 100, input_type = "radio", option_text = "No", order = 2 },
+
+                    //Standard Additional Information: Show Gender
+                    new question_options() { id = 241, id_question = 101, input_type = "radio", option_text = "Yes", order = 1 },
+                    new question_options() { id = 242, id_question = 101, input_type = "radio", option_text = "No", order = 2 },
+
+                    //Standard Additional Information: Show Dimensions
+                    new question_options() { id = 243, id_question = 102, input_type = "radio", option_text = "Yes", order = 1 },
+                    new question_options() { id = 244, id_question = 102, input_type = "radio", option_text = "No", order = 2 },
+
+                    //Standard Additional Information: Show Product Group
+                    new question_options() { id = 245, id_question = 103, input_type = "radio", option_text = "Yes", order = 1 },
+                    new question_options() { id = 246, id_question = 103, input_type = "radio", option_text = "No", order = 2 },
+
+                    //Standard Additional Information: Show Warehouse
+                    new question_options() { id = 247, id_question = 104, input_type = "radio", option_text = "Yes", order = 1 },
+                    new question_options() { id = 248, id_question = 104, input_type = "radio", option_text = "No", order = 2 },
+
+                    //Standard Additional Information: Show Product Type?
+                    new question_options() { id = 249, id_question = 105, input_type = "radio", option_text = "Yes", order = 1 },
+                    new question_options() { id = 250, id_question = 105, input_type = "radio", option_text = "No", order = 2 }
 
             );
         }
