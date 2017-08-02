5f49329d5b83710e18cb7e8513a335b82a4e031d
César M. Urdaneta <upcesar@gmail.com>
2017-08-02 00:04:14 -0300

    Added more question to Section 9

--- cleangap.api/Migrations/Seed/Questions/SeedQuestionValidation.cs
+++ cleangap.api/Migrations/Seed/Questions/SeedQuestionValidation.cs
@@ -12,10 +12,39 @@
     {
         private void SeedQuestionValidation(DAL.CleanGapDataContext context)
         {
-            question_sections qsHeader = context.question_sections.Find(9);
+            question_sections qsHeader = context.question_sections.Find(9);     //Validation
 
-            
+            SeedQuestionValidationHeader(context, qsHeader);
+            SeedQuestionValidationSubmission(context, qsHeader);
 
+        }
+
+        private void SeedQuestionValidationHeader(DAL.CleanGapDataContext context, question_sections qsHeader)
+        {
+
+            question_sections subSection = context.question_sections.Find(28);  //Header Validation
+
+            context.questions
+                   .AddOrUpdate(
+                        new questions() { id = 119, name = "validation_header_po", description = "Do you require a PO?", page = 30, question_sections = qsHeader, id_section = qsHeader.id, id_subsection = subSection.id },
+                        new questions() { id = 120, name = "validation_header_def", description = "If any fields are not defaulted, do you require them to be filled out?", page = 30, question_sections = qsHeader, id_section = qsHeader.id, id_subsection = subSection.id },
+                        new questions() { id = 121, name = "validation_header_cust", description = "Customer", page = 30, question_sections = qsHeader, parent_question_id = 120, parent_answer_value = "Yes", id_section = qsHeader.id, id_subsection = subSection.id },
+                        new questions() { id = 122, name = "validation_header_cdate", description = "Cancel date", page = 30, question_sections = qsHeader, parent_question_id = 120, parent_answer_value = "Yes", id_section = qsHeader.id, id_subsection = subSection.id },
+                        new questions() { id = 123, name = "validation_header_sdate", description = "Start date", page = 30, question_sections = qsHeader, parent_question_id = 120, parent_answer_value = "Yes", id_section = qsHeader.id, id_subsection = subSection.id }
+            );
+
+        }
+
+        private void SeedQuestionValidationSubmission(DAL.CleanGapDataContext context, question_sections qsHeader)
+        {
+            question_sections subSection = context.question_sections.Find(28);  //Header Validation
+
+            context.questions
+                   .AddOrUpdate(
+                        new questions() { id = 124, name = "validation_submit_inv", description = "Do you want to validate for Inventory Availability?", page = 31, question_sections = qsHeader, id_section = qsHeader.id, id_subsection = subSection.id },
+                        new questions() { id = 125, name = "validation_submit_spec", description = "Are there specific order types that do not require inventory validation?", page = 31, parent_question_id = 124, parent_answer_value = "Yes", question_sections = qsHeader, id_section = qsHeader.id, id_subsection = subSection.id },
+                        new questions() { id = 126, name = "validation_submit_cust", description = "Customer", page = 30, question_sections = qsHeader, parent_question_id = 120, parent_answer_value = "Yes", id_section = qsHeader.id, id_subsection = subSection.id }
+            );
         }
     }
 }
